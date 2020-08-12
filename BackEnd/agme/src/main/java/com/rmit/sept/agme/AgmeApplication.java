7eb760fa71a9e2a47a56d63fc394a86e1aa0b87d
Luke <41284505+s3725331@users.noreply.github.com>
2020-08-12 15:07:27 +1000

    Initial commit of Spring Boot backend as shown in SEPT tutorials Template entity and corresponding repo,service,controller added

---
+++ BackEnd/agme/src/main/java/com/rmit/sept/agme/AgmeApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.agme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgmeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/agme/src/test/java/com/rmit/sept/agme/AgmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.agme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
