606ca3947ac12e5874dc404abf1f55f93b54fe02
Cass Ilangantileke s3742928 <s3742928@student.rmit.edu.au>
2020-08-16 20:30:58 +1000

    Passing tests for AccountServices

---
+++ BackEnd/agme/src/test/java/com/rmit/sept/agme/AccountServiceTests.java
@@ -0,0 +1,66 @@
+package com.rmit.sept.agme;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import com.rmit.sept.agme.model.Account;
+import com.rmit.sept.agme.repositories.AccountRepository;
+import com.rmit.sept.agme.services.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AccountServiceTests {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    public void testAccountServiceGet() {
+        Account account = new Account();
+        account.setEmail("bob@gmail.com");
+        account.setPassword("password123");
+        account.setFirstName("Bob");
+        account.setLastName("Build");
+        account.setAddress("26 Hill Avenue EastLand");
+
+        Account secondAccount = accountRepository.save(account);
+
+        assertTrue(accountService.get(secondAccount.getId()).isPresent());
+
+    }
+
+
+    @Test
+    public void testAccountServiceUpdate() {
+        Account account = new Account();
+        account.setEmail("dominic@gmail.com");
+        account.setPassword("password123");
+        account.setFirstName("Dominic");
+        account.setLastName("Person");
+        account.setAddress("2 Wayne Street Etihad");
+
+        Account secondAccount = accountRepository.save(account);
+
+        assertTrue(accountService.update(secondAccount).isPresent());
+
+    }
+
+    @Test
+    public void testAccountServiceCreate() {
+        Account account = new Account();
+        account.setEmail("ashley@gmail.com");
+        account.setPassword("password123");
+        account.setFirstName("Ashley");
+        account.setLastName("Dale");
+        account.setAddress("34 Albert Road Tower");
+
+        Account secondAccount = accountRepository.save(account);
+
+        assertTrue(accountService.create(secondAccount).isPresent());
+
+    }
+}
