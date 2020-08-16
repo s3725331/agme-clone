b0e76eddc48e30c0db828cee29c26d5e73e6afd3
ChrisGitian <s3783461@student.rmit.edu.au>
2020-08-16 23:40:00 +1000

    unit testing for customers/workers

--- BackEnd/agme/src/test/java/com/rmit/sept/agme/CustomerServiceTests.java
+++ BackEnd/agme/src/test/java/com/rmit/sept/agme/CustomerServiceTests.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CustomerServiceTests {
@@ -27,32 +26,80 @@ public class CustomerServiceTests {
     @Test
     public void testValidCustomerCreate() {
         Account cust = new Account();
-        CustomerService cuServ = new CustomerService();
         cust.setAddress("13 realplace drive, suburbs");
         cust.setEmail("Juan@mail.com");
         cust.setFirstName("Juan");
         cust.setLastName("Rosso");
         cust.setPassword("password");
-//        cust.setId(12345);
         Account newAccount = accountRepository.save(cust);
-//        Customer customer = new Customer(cust);
-//        Customer newCustomer = customerRepository.save(customer);
         assertTrue(customerService.create(newAccount.getId()).isPresent());
     }
 
     @Test
     public void testInvalidCustomerCreate() {
         Account cust = new Account();
-        CustomerService cuServ = new CustomerService();
         cust.setAddress("13 realplace drive, suburbs");
         cust.setEmail("Juan@mail.com");
         cust.setFirstName("Juan");
         cust.setLastName("Rosso");
         cust.setPassword("password");
-//        cust.setId(12345);
         Account newAccount = accountRepository.save(cust);
-//        Customer customer = new Customer(cust);
-//        Customer newCustomer = customerRepository.save(customer);
         assertFalse(customerService.create(12345).isPresent());
     }
+    @Test
+    public void testValidCustomerGet() {
+        Account cust = new Account();
+        cust.setAddress("13 realplace drive, suburbs");
+        cust.setEmail("Juan@mail.com");
+        cust.setFirstName("Juan");
+        cust.setLastName("Rosso");
+        cust.setPassword("password");
+        Account newAccount = accountRepository.save(cust);
+        Customer customer = new Customer(newAccount);
+        Customer newCustomer = customerRepository.save(customer);
+        assertTrue(customerService.get(newCustomer.getId()).isPresent());
+    }
+
+    @Test
+    public void testInvalidCustomerGet() {
+        Account cust = new Account();
+        cust.setAddress("13 realplace drive, suburbs");
+        cust.setEmail("Juan@mail.com");
+        cust.setFirstName("Juan");
+        cust.setLastName("Rosso");
+        cust.setPassword("password");
+        Account newAccount = accountRepository.save(cust);
+        Customer customer = new Customer(newAccount);
+        Customer newCustomer = customerRepository.save(customer);
+        assertFalse(customerService.get(12345).isPresent());
+    }
+
+    @Test
+    public void testValidCustomerUpdate() {
+        Account cust = new Account();
+        cust.setAddress("13 realplace drive, suburbs");
+        cust.setEmail("Juan@mail.com");
+        cust.setFirstName("Juan");
+        cust.setLastName("Rosso");
+        cust.setPassword("password");
+        Account newAccount = accountRepository.save(cust);
+        Customer customer = new Customer(newAccount);
+        Customer newCustomer = customerRepository.save(customer);
+        assertEquals(customerService.update(newCustomer).get(),newCustomer);
+    }
+
+    @Test
+    public void testInvalidCustomerUpdate() {
+        Account cust = new Account();
+        cust.setAddress("13 realplace drive, suburbs");
+        cust.setEmail("Juan@mail.com");
+        cust.setFirstName("Juan");
+        cust.setLastName("Rosso");
+        cust.setPassword("password");
+        Account newAccount = accountRepository.save(cust);
+        Customer customer = new Customer(newAccount);
+        Customer newCustomer = customerRepository.save(customer);
+        assertNotEquals(customerService.update(new Customer(cust)),newCustomer);
+    }
+
 }
