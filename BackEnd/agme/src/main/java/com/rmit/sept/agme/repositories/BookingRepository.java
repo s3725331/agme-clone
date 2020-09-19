9175d4963ad4f739a9fc24546c352a3cba1be08c
Luke <41284505+s3725331@users.noreply.github.com>
2020-09-19 12:17:49 +1000

    bookings bug fixes

--- BackEnd/agme/src/main/java/com/rmit/sept/agme/repositories/BookingRepository.java
+++ BackEnd/agme/src/main/java/com/rmit/sept/agme/repositories/BookingRepository.java
@@ -12,15 +12,15 @@ import java.util.List;
 
 public interface BookingRepository extends CrudRepository<Booking, Long> {
 
-    @Query("SELECT b FROM Booking b WHERE b.worker = (:workerID) AND b.startTime > (:time)")
+    @Query("SELECT b FROM Booking b WHERE b.worker.id = (:workerID) AND b.startTime > (:time)")
     List<Booking> getUpcomingByWorker(@Param("workerID")long workerID, @Param("time") Date time);
 
-    @Query("SELECT b FROM Booking b WHERE b.customer = (:customerID) AND b.startTime > (:time)")
+    @Query("SELECT b FROM Booking b WHERE b.customer.id = (:customerID) AND b.startTime > (:time)")
     List<Booking> getUpcomingByCustomer(@Param("customerID")long customerID, @Param("time") Date time);
 
-    @Query("SELECT b FROM Booking b WHERE b.worker = (:workerID) AND b.startTime < (:time)")
+    @Query("SELECT b FROM Booking b WHERE b.worker.id = (:workerID) AND b.startTime < (:time)")
     List<Booking> getPastByWorker(@Param("workerID")long workerID, @Param("time") Date time);
 
-    @Query("SELECT b FROM Booking b WHERE b.customer = (:customerID) AND b.startTime < (:time)")
+    @Query("SELECT b FROM Booking b WHERE b.customer.id = (:customerID) AND b.startTime < (:time)")
     List<Booking> getPastByCustomer(@Param("customerID") long customerID, @Param("time") Date time);
 }
