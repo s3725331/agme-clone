47f01604edd92fe52464a391ff36e007a047d9ec
[Kevin Nguyen s3783669] <s3783668@student.rmit.edu.au>
2020-09-19 17:15:25 +1000

    footer updated

--- FrontEnd/agme_react/my-app/src/Components/Layout/Footer.js
+++ FrontEnd/agme_react/my-app/src/Components/Layout/Footer.js
@@ -9,15 +9,15 @@ export default class Footer extends Component {
         <div class="container">
           <div class="row">
             <div class="col l6 s12">
-              <h5 class="white-text">Footer Content</h5>
-              <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              <a class="white-text" href=""><h6><i className="material-icons">collections_bookmark</i>Agme Booking</h6></a>
+              <p class="grey-text text-lighten-4"></p>
             </div>
             <div class="col l4 offset-l2 s12">
               
               <ul>
                 <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
                 <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
-                <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/kevin-nguyen-2270331ab/">LinkedIn</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">LinkedIn</a></li>
                 <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
               </ul>
             </div>
