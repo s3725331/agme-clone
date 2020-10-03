49ffa65ff6cdf7de14d8584d8882c5fbe04a9e15
Luke <41284505+s3725331@users.noreply.github.com>
2020-10-03 22:05:54 +1000

    user name patch

--- BackEnd/agme/src/main/resources/data.sql
+++ BackEnd/agme/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (id, username, password, full_name) VALUES
-  (1, 'admin@agme', 'admin1234', 'admin');
+INSERT INTO user (id, username, password, first_name, last_name, address) VALUES
+  (1, 'admin@agme', 'admin1234', 'admin', 'user', 'na');
 
 INSERT INTO admin (id, user_id) VALUES
   (1, (SELECT id from user WHERE id=1));
