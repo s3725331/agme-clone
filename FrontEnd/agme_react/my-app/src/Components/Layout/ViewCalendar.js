85b9d9c6df5193759f95ce098600392a6beeb739
s3784244 <s3784244@student.rmit.edu.au>
2020-10-05 13:16:42 +1100

    Updated calender functionality for workers

--- FrontEnd/agme_react/my-app/src/Components/Layout/ViewCalendar.js
+++ FrontEnd/agme_react/my-app/src/Components/Layout/ViewCalendar.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { Inject,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda } from '@syncfusion/ej2-react-schedule';
+import { Inject,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda, ResourceDirective, ResourcesDirective } 
+         from '@syncfusion/ej2-react-schedule';
 
 
 export default class ViewCalendar extends Component {
+   localData = [
+  {
+     Id: 1,
+     Subject: 'Appointment',
+     StartTime: new Date(2020, 4, 8, 6, 0),
+     EndTime: new Date(2020, 4, 8, 7, 0)
+   },
+   {
+     Id: 2,
+     Subject: 'Consultantcy',
+     StartTime: new Date(2020, 4, 10, 6, 0),
+     EndTime: new Date(2020, 4, 10, 7, 0)
+  }];
      
   render() { 
 
-    return <ScheduleComponent> 
+    return <ScheduleComponent currentView='Month' eventSettings={{ dataSource: this.localData }}> 
+          <ResourcesDirective>
+            <ResourceDirective field='ResourceID' title='Worker Name' name='Resources' textField='Name' idField='Id' colorField='Colour'></ResourceDirective>
+          </ResourcesDirective>
         <Inject services={[Day,Week,WorkWeek,Month,Agenda]}/>
     </ScheduleComponent>
     
